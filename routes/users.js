c149be29343a5cd77d051d181695c6106da3ac60
guidoAlreadyTaken <chris.baer9105@gmail.com>
2018-02-28 10:55:12 +0100

    Route create user

--- routes/users.js
+++ routes/users.js
@@ -13,4 +13,20 @@ router.get('/', function(req, res, next) {
   });
 });
 
+/* POST new user */
+router.post('/', function(req, res, next) {
+	// Create a new document from the JSON in request body
+	const newUser = new User(req.body);
+
+	// Sauver dans la BD le new document
+	newUser.save(function(err, savedUser) {
+		if (err) {
+			return next(err);
+		}
+
+		// Envoi du document sauver
+		res.send(savedUser);
+	});
+});
+
 module.exports = router;
